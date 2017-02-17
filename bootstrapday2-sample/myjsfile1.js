cabc313adae33bbe863f405e6fdef636c3e96ca4
falconflyer87 <samsonolive@hotmail.com>
2017-02-17 16:31:38 -0700

    Commiting updates to tree with new directories and files.

---
+++ bootstrapday2-sample/myjsfile1.js
@@ -0,0 +1,5 @@
+/**
+ * Created by mac on 2/17/17.
+ */
+document.body.appendChild(document.createTextNode('Hello World!'));
+var h1 = document.getElementById('header'); // holds a reference to the <h1> tag
